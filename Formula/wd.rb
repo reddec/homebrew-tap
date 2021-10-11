1694cce18a68ebd765e400185870373cbaaa83ab
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-11 20:09:13 +0800

    Brew formula update for wd version v0.0.10

--- Formula/wd.rb
+++ Formula/wd.rb
@@ -5,33 +5,33 @@
 class Wd < Formula
   desc "Yet another webhook daemon"
   homepage "https://github.com/reddec/wd"
-  version "0.0.9"
+  version "0.0.10"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/reddec/wd/releases/download/v0.0.9/wd_0.0.9_darwin_amd64.tar.gz"
-      sha256 "695cbe9353af6ef365366a62f642243f93aefa16d6cdb2efbec03f2f67bc04be"
+      url "https://github.com/reddec/wd/releases/download/v0.0.10/wd_0.0.10_darwin_amd64.tar.gz"
+      sha256 "d3f7e98998cbc21fe428a33c021bda834db25a4355ca1ea5ed094436d5887ec1"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/reddec/wd/releases/download/v0.0.9/wd_0.0.9_darwin_arm64.tar.gz"
-      sha256 "53d7e5670fed313d349250a9999dd7c4098e7d4346ba2aeb0d874d9621c31ae0"
+      url "https://github.com/reddec/wd/releases/download/v0.0.10/wd_0.0.10_darwin_arm64.tar.gz"
+      sha256 "aa9c8b031177ca0de9a5f4aed029a2e27836d6eb52d44b89bf5a2379a8672113"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/reddec/wd/releases/download/v0.0.9/wd_0.0.9_linux_armv6.tar.gz"
-      sha256 "7db563a493c47711beeae168913020214ce225b4d65e54f33f53a5d22da8f480"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/reddec/wd/releases/download/v0.0.9/wd_0.0.9_linux_amd64.tar.gz"
-      sha256 "8eaaa497dab9f6cecb903e47b6f589cbac25de9b877b616378ea907e91809d36"
+      url "https://github.com/reddec/wd/releases/download/v0.0.10/wd_0.0.10_linux_amd64.tar.gz"
+      sha256 "38f3da4312ff0dc6cf7289778ffcdece7f56e980ec13f4a6097619a947b0b529"
+    end
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/reddec/wd/releases/download/v0.0.10/wd_0.0.10_linux_armv6.tar.gz"
+      sha256 "a3af075f32b41748840bbdabc95c0e207ef21f3aa04ce8b87d5a324a2a0c46a4"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/reddec/wd/releases/download/v0.0.9/wd_0.0.9_linux_arm64.tar.gz"
-      sha256 "4c0946cb32b3a2499d146dab33aca20cca99e775da74782a611184daf3226e46"
+      url "https://github.com/reddec/wd/releases/download/v0.0.10/wd_0.0.10_linux_arm64.tar.gz"
+      sha256 "e7869e53d04b81169f0a70fac448e749e1cea9880c4fcd6d3c064eb637cd2830"
     end
   end
 
